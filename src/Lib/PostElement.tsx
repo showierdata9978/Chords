2f0d0465848339d32542c4311bf757fc20e04245
ShowierData9978 <mellfang36@gmail.com>
2024-04-02 23:18:20 -0500

    Feat: Make Posts display Pfps **Fix: styling issues** **Fix: Logging in errors**

--- src/Lib/PostElement.tsx
+++ src/Lib/PostElement.tsx
@@ -1,8 +1,9 @@
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import type { Post } from '@meower-media/meower/dist/api/posts';
 import { Image, ImageRequireSource, StyleSheet, Text, View } from 'react-native';
 import colors from '../Colors';
+import ClientContext from '../context/client';
 
 type Props = {
     post: Post;
@@ -21,28 +22,56 @@ const styles = StyleSheet.create({
     text: {
         fontSize: 20,
         color: colors.foreground,
+        flexWrap: "nowrap"
     },
     username: {
         fontSize: 21,
         color: colors.foreground,
         fontStyle: 'italic'
     },
+    pfp: {
+        width: 50,
+        height: 50,
+        borderRadius: 25,
+
+    }
 });
 
 
 
-function Badge({src}: {src: ImageRequireSource}) {
+function Badge({ src }: { src: ImageRequireSource }) {
     return (
-        <Image source={src} style={{width: 25, height: 25, margin: 5}} />
+        <Image source={src} style={{ width: 25, height: 25, margin: 5 }} />
     )
 }
 
 
-export default function Post({ post }: Props)  {
+export default function Post({ post }: Props) {
+    const client = React.useContext(ClientContext);
+    const [pfpUrl, setPfpUrl] = React.useState<string>("https://3r1s-s.github.io/meo/images/avatars-webp/icon_-2.svg");
+
+    useEffect(() => { }, [pfpUrl]);
+    client.api.users.get(post.u).then((user) => {
+        if (user.body.error) return;
+
+        if (user.body.avatar !== null) {
+            setPfpUrl(`https://uploads.meower.org/icons/${user.body.avatar}`);
+            return;
+        }
+        setPfpUrl(`https://3r1s-s.github.io/meo/images/avatars-webp/icon_${user.body.pfp_data}.webp`);
+    }).catch((err) => { });
+
     return (
         <View style={styles.container}>
-            <Text style={styles.username}> {post.u} {post.bridged && post.bridged.u === "Discord" ?  <Badge src={require("../assets/discord.png")} /> : <View/>} </Text> 
+            <View style={{
+                flexDirection: 'row',
+                flexWrap: 'wrap',
+            }}>
+                <Image src={pfpUrl} style={styles.pfp} />
+                <Text style={styles.username}> {post.u} {post.bridged && post.bridged.u === "Discord" ? <Badge src={require("../assets/discord.png")} /> : <View />} </Text>
+            </View>
             <Text style={styles.text}>{post.p}</Text>
+
         </View>
     )
 }
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -18,7 +18,7 @@ const styles = StyleSheet.create({
         color: '#FFFFFF',
     },
 
-      button: {
+    button: {
         width: "95%",
         height: 50,
         justifyContent: 'center',
@@ -28,7 +28,19 @@ const styles = StyleSheet.create({
         borderWidth: 2,
         borderColor: colors.foreground,
         margin: 10
-     }
+    },
+    align: {
+        width: "95%",
+        height: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin: 10
+    },
+    textInput: {
+        borderStyle: 'dotted',
+        borderColor: colors.orange,
+        borderWidth: 1,
+    }
 });
 
 export default function Login({ navigation }: LoginProps) {
@@ -42,36 +54,40 @@ export default function Login({ navigation }: LoginProps) {
             if (typeof client.user === 'undefined' || client.user === null) {
                 setError('Failed to login. Please try again.');
                 return;
-            
             };
             client.off('login', loginListener);
+            setError("You have logged in successfully!")
             navigation.reset({
                 index: 0,
                 routes: [{ name: 'home' }],
             });
         };
 
+        const errorListener = (err: Error) => {
+            setError('Failed to login. Please try again.');
+        }
+
         client.onLogin(loginListener);
+        client.on('.error', errorListener);
         return () => {
             client.off('login', loginListener);
+            client.off('.error', errorListener);
         }
-    });
+    }, []);
 
     return (
         <View style={styles.container}>
             <Text>Login</Text>
-            <TextInput placeholder="Username" maxLength={20} autoComplete="username" onChange={
+            <TextInput style={{...styles.textInput, ...styles.align}} placeholder="Username" maxLength={20} autoComplete="username" onChange={
                 (e) => {
                     setUsername(e.nativeEvent.text)
                 }
-            
-            }/>
-            <TextInput secureTextEntry={true} placeholder="Password" autoComplete="password" onChange={
+            } />
+            <TextInput style={{...styles.textInput, ...styles.align}} secureTextEntry={true} placeholder="Password" autoComplete="password" onChange={
                 (e) => {
                     setPassword(e.nativeEvent.text)
                 }
-            }/>
-
+            } />
             <TouchableOpacity
                 style={styles.button}
                 onPress={() => {
@@ -79,11 +95,9 @@ export default function Login({ navigation }: LoginProps) {
 
                 }}
             >
-                 <Text style={styles.text}>Login</Text>
+                <Text style={styles.text}>Login</Text>
             </TouchableOpacity>
-            
-
-            {error !== "" ? <Text>{error}</Text> : <View/>}
+            {error !== "" ? <Text style={styles.align}>{error}</Text> : <View />}
         </View>
     )
 }
